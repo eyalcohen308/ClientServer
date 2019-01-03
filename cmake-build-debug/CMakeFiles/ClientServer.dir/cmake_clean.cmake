5a121dfd81113c07870b5e4cbb0ece877e581cbe
Eyal <eyalcohen308@gmail.com>
2019-01-03 14:27:07 +0200

    begin

---
+++ cmake-build-debug/CMakeFiles/ClientServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClientServer.dir/main.cpp.o"
+  "ClientServer.pdb"
+  "ClientServer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ClientServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
